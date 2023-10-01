9983803287ee22082af70744d02dd4cd9316461a
dobroillya <dobroillya@gmail.com>
2023-10-01 18:35:04 +0300

    data fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const UsersPage = () => {
+type User = {
+  id: number;
+  name: string;
+};
+
+const UsersPage = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: User[] = await response.json();
+
   return (
-    <div>UsersPage</div>
-  )
-}
+    <>
+      <div>UsersPage</div>
+      <ul>{users.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
+    </>
+  );
+};
 
-export default UsersPage
+export default UsersPage;
