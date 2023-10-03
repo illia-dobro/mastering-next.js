7657c767c56394ba19f06ecae6c26aab1674c1fb
dobroillya <dobroillya@gmail.com>
2023-10-03 17:26:26 +0300

    create data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,13 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/prisma/client";
 
-export function GET(request: NextRequest){
-  return NextResponse.json([
-    {id:1, name: 'john'},
-    {id:2, name: 'garry'},
-  ])
+export async function GET(request: NextRequest){
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users)
 }
 
 export async function POST(request: NextRequest){
   const body = await request.json();
-  return NextResponse.json(body);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email
+    }
+  })
+
+  if(user){
+    return NextResponse.json({error: 'User already exists'}, {status: 400})
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email
+    }
+  })
+  return NextResponse.json(newUser, {status: 201});
 }
