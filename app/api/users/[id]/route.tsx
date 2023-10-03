c55f912ab66b37544eb4d293651b51f7c5ee7bfd
dobroillya <dobroillya@gmail.com>
2023-10-03 15:18:20 +0300

    api getting single obj

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { json } from 'stream/consumers';
+
+
+
+export function GET(request: NextRequest, { params }: { params: {id: number} } ) {
+  if(params.id > 10) {
+    return NextResponse.json({error: 'User not found'}, { status: 404})
+  }
+  return NextResponse.json({id: '1', name: 'mosh'})
+}
