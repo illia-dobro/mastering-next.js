f8298df02ffa7b90e36127d50e5077d987e9614b
dobroillya <dobroillya@gmail.com>
2023-10-04 16:22:11 +0300

    signin signup

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,41 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { prisma } from "@/prisma/client";
+import bcrypt from 'bcrypt'
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: 'Credential',
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: { label: "Password", type: "password" },
+    },
+    async authorize(credentials, req) {
+      if(!credentials?.email || credentials.password){
+        return null
+      } else {
+        const user = await prisma.user.findUnique({where: {email: credentials.email}})
+        if(!user) {
+          return null
+        } else {
+          const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword!)
+          return passwordsMatch ? user : null
+        }
+      }
+    }
+  }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
     })
-  ]
+  ],
+  session: {
+    strategy: 'jwt'
+  }
 }
 
 const handler = NextAuth(authOptions)
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
@@ -13,7 +13,12 @@ const Navbar = () => {
   return (
     <>
       <Link href="/users">Users</Link>
-      {status === 'authenticated' && <div>{session.user?.name}</div>}
+      {status === 'authenticated' && (
+        <div>
+          {session.user?.name}
+          <Link href='/api/auth/signout'>Sign Out</Link>
+        </div>
+      )}
       {status === 'unauthenticated' && (
         <div>
           <Link href="/api/auth/signin">Sign-in</Link>
