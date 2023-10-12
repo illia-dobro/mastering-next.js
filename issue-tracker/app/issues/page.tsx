32bc60c311429476e814c58902a4a74f87a1503f
dobroillya <dobroillya@gmail.com>
2023-10-12 19:27:27 +0300

    + redix ui

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@radix-ui/themes'
 import React from 'react'
 
 const IssuesPage = () => {
   return (
     <div>
-      ss
+      <Button>New issue</Button>
     </div>
   )
 }
