f0d0bff56e218cf6a25c562832daae3096584712
dobroillya <dobroillya@gmail.com>
2023-10-12 17:38:05 +0300

    style active link in navbar

---
+++ issue-tracker/app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IssuesPage = () => {
+  return (
+    <div>
+      ss
+    </div>
+  )
+}
+
+export default IssuesPage
---
+++ issue-tracker/app/nav-bar.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import { AiFillBug } from 'react-icons/ai';
+import classNames from 'classnames';
+
+const links = [
+  { label: 'Dashboard', href: '/' },
+  { label: 'Issues', href: '/issues' },
+];
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  return (
+    <nav className="mx-auto max-w-7xl sm:px-6 lg:px-8 flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => {
+          return (
+            <li key={link.label}>
+              <Link
+                className={classNames({
+                  'text-zinc-900' : link.href === currentPath,
+                  'text-zinc-500' : link.href !== currentPath,
+                  'hover:text-zinc-800 transition-colors': true
+                })}
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
